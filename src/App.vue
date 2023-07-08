38c49e392c0ed72513e173c05fdf7f273f84f25c
lucasferreiralimax <lucasferreiralimax@gmail.com>
2023-07-08 16:16:54 -0300

    adjustment title

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,38 @@
 <script setup>
 import { ref } from 'vue'
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 const drawer = ref(false)
 </script>
 
 <template>
-    <!-- <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div> -->
-    <v-layout>
-      <v-app-bar
-        color="primary"
-        prominent
-      >
-        <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+  <v-layout>
+    <v-app-bar
+      color="primary"
+      prominent
+    >
+      <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer" />
+      <v-toolbar-title>Exam-Digitalz</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn variant="text" icon="mdi-magnify"></v-btn>
+      <v-btn variant="text" icon="mdi-filter"></v-btn>
+      <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
+    </v-app-bar>
 
-        <v-toolbar-title>My exam</v-toolbar-title>
-
-        <v-spacer></v-spacer>
-
-        <v-btn variant="text" icon="mdi-magnify"></v-btn>
-
-        <v-btn variant="text" icon="mdi-filter"></v-btn>
-
-        <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
-      </v-app-bar>
-
-      <v-main style="height: 500px;">
-        <v-card-text>
-          <RouterView />
-        </v-card-text>
-      </v-main>
-    </v-layout>
+    <v-navigation-drawer
+      v-model="drawer"
+      location="left"
+      temporary
+    >
+      <v-list density="compact" nav>
+        <v-list-item prepend-icon="mdi-home-city" title="Home" value="home" to="/"></v-list-item>
+        <v-list-item prepend-icon="mdi-account" title="My Account" value="account" to="/about"></v-list-item>
+        <v-list-item prepend-icon="mdi-account-group-outline" title="Users" value="users"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-main>
+      <v-card-text>
+        <RouterView />
+      </v-card-text>
+    </v-main>
+  </v-layout>
 </template>
-
-<style>
-* {
-  color: #000;
-}
-</style>
